cc6b4a2b8c69636eb06d59dc4c17f07ae9220969
Tanner Hornsby <tannerhornsby7@gmail.com>
2022-09-08 00:48:24 -0500

    Create Populate Fn's for each section

--- src/layout.js
+++ src/layout.js
@@ -9,28 +9,15 @@ let layout = (c, h, n, m, f) => {
         n, //navbar
         m, //main
         compose () {
-            return appendSections(c, h, n, m)
+            appendSections(c, h, n, m);
+            populateHead();
+            populateNav();
+            populateMain();
+            return;
         }
     }
-
     return Object.assign(Object.create(proto), { c, h, n, m } );
 };
-
-// Create Header
-
-
-
-// Create Navbar
-
-// Create DOM section and add it's class
-function createSection (section) {
-    let dome = document.createElement('div');
-    if(section != "container") {
-        dome.textContent = section;
-    }
-    dome.classList.add(section);
-    return dome;
-}
 
 // Append list of sections to container
 function appendSections(c) {
@@ -40,5 +27,51 @@ function appendSections(c) {
     }
     return document.body.appendChild(container);
 }
+
+// Create DOM section and add it's class
+function createSection (section) {
+    let dome = document.createElement('div');
+
+    dome.classList.add(section);
+    return dome;
+}
+
+// populate Header
+function populateHead() {
+
+}
+
+// populate NavBar
+function populateNav() {
+    const nav = document.querySelector('.navbar');
+    const projects = document.createElement('h2');
+    projects.innerHTML = 'PROJECTS';
+
+    //appending children to nav
+    nav.appendChild(createButton("inbox"));
+    nav.appendChild(createButton("day"));
+    nav.appendChild(createButton("week"));
+    nav.appendChild(projects);
+    return;
+}
+
+// populate Main
+function populateMain() {
+    return;
+}
+
+// create Button
+function createButton(name) {
+    const element = document.createElement('button');
+    const icon = document.createElement('ion-icon');
+
+    element.appendChild(icon);
+    element.innerHTML = name.toUpperCase();
+    element.id = name;
+    // icon.href = `url(${name})`;
+
+    return element;
+}
+
 
 export default layout;
